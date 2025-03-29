b5ca6d7cd362f0403082293cf25f31cf37a20fef
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-30 00:47:45 +0300

    Add getWeeklyTrends function to fetch trending movies using axios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-const get
+import axios from 'axios';
+
+const API_KEY = 'e159d731039dcacac75f0d6a2d5371ca';
+
+const getWeeklyTrends = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/trending/all/day',
+        params: { language: 'en-US' },
+        headers: {
+          accept: 'application/json',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTU5ZDczMTAzOWRjYWNhYzc1ZjBkNmEyZDUzNzFjYSIsIm5iZiI6MTc0MzIwMzMwOC4zMTQsInN1YiI6IjY3ZTcyYmVjMGU4ZWU2NzgxNTY3YTQ4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H1VcIsy5PEjzy4uHm47ss9XozIyh5LIka9hEmPAOO3k',
+        },
+      };
+
+      axios
+        .request(options)
+        .then(res => console.log(res.data))
+        .catch(err => console.error(err));
+    } catch (error) {
+      console.error('getWeeklyTrends Error:', error);
+    }
+  });
+};
+
+getWeeklyTrends();
