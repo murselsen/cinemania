d3dc48fcec76da17977b29d6a4fc883ed2f8d3a2
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-31 00:01:41 +0300

    Add console log for film details and update title rendering in trends.js

--- src/js/trends.js
+++ src/js/trends.js
@@ -5,12 +5,15 @@ const weeklyTrendsList = document.querySelector('#weekly-trends-list');
 getWeeklyTrends().then(res => {
   for (let index = 0; index < 3; index++) {
     const film = res[index];
+    console.log('Film:', film);
     weeklyTrendsList.innerHTML += `<li class="stand__area-item">
-            <img src="https://image.tmdb.org/t/p/original/${film.backdrop_path}" alt="GHOSTED" title="GHOSTED">
+            <img src="https://image.tmdb.org/t/p/original/${film.backdrop_path
+      }" alt="GHOSTED" title="GHOSTED">
             <div class="stand__area-item-content">
-              <h3 class="stand__area-item-title">GHOSTED</h3>
+              <h3 class="stand__area-item-title">${film.original_name || film.original_title
+      }</h3>
               <div class="stand__area-item-content-footer">
-                <p class="stand__area-item-info">Drama, Action | 2023</p>
+                <p class="stand__area-item-info">${film.} | 2023</p>
                 <span class="stand__area-item-rating">
                   <span class="star star"></span>
                   <span class="star star"></span>
