1fcfb101df3761ad6300b4fee06fb39d99cd585b
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-31 00:49:29 +0300

    Add genre mapping and update film data structure in trends.js

--- src/js/trends.js
+++ src/js/trends.js
@@ -2,13 +2,42 @@ import { getWeeklyTrends } from './tmdb.js';
 
 const weeklyTrendsList = document.querySelector('#weekly-trends-list');
 
+const genreMap = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+};
+
 getWeeklyTrends().then(res => {
   for (let index = 0; index < 3; index++) {
     const film = res[index];
     console.log('Film:', film);
     const filmData = {
+      name: film.original_name || film.original_title,
+      poster_path: film.poster_path,
+      year: 1,
+      tags: film.genre_ids
+        .map(genreId => genreMap[genreId] || 'Unknown')
+        .flat(),
+    };
 
-    }
+    console.log('Film Data:', filmData);
     weeklyTrendsList.innerHTML += `<li class="stand__area-item">
             <img src="https://image.tmdb.org/t/p/original/${
               film.poster_path
@@ -18,9 +47,7 @@ getWeeklyTrends().then(res => {
                 film.original_name || film.original_title
               }</h3>
               <div class="stand__area-item-content-footer">
-                <p class="stand__area-item-info"> | ${
-                  film.release_date.split('-')[0]
-                }</p>
+                <p class="stand__area-item-info"> | 2023</p>
                 <span class="stand__area-item-rating">
                   <span class="star star"></span>
                   <span class="star star"></span>
