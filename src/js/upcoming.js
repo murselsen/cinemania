bca860e897b0fe2ea487b2d5d57920546b956c65
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-02 00:32:08 +0300

    Set alt attribute for upcoming movie image based on film title

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -4,7 +4,8 @@ export const upComingRender = async film => {
     console.log('film:', film);
     const upcomingImage = document.createElement('img');
     upcomingImage.src = `https://image.tmdb.org/t/p/original/${film.backdrop_path}`;
-    up
+    upcomingImage.alt = film.original_name || film.original_title;
+    upcomingImage.style.width
   } catch (error) {
     console.error('Error in upcomingRender:', error);
   }
