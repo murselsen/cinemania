6e31daf0ca43332fdf99c74b9daad55e6159777e
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-02 00:43:33 +0300

    Add styling for upcoming image in desktop.css

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -2,13 +2,14 @@ export const upComingRender = async film => {
   try {
     console.log('upcomingRender function called');
     console.log('film:', film);
-    const stand__area-upcoming
+    const stand__areaUpcoming = document.querySelector('#stand__area-upcoming');
     // Image elements
     const upcomingImage = document.createElement('img');
     upcomingImage.src = `https://image.tmdb.org/t/p/original/${film.backdrop_path}`;
     upcomingImage.alt = film.original_name || film.original_title;
-    upcomingInfoArea.classList.add('stand__area-upcoming-image');
+    upcomingImage.classList.add('stand__area-upcoming-image');
 
+    stand__areaUpcoming.appendChild(upcomingImage);
     // UpComing Info Area
     const upcomingInfoArea = document.createElement('div');
     upcomingInfoArea.classList.add('stand__area-upcoming-info');
