1a32e8b4c944579370a183ceefb6e77e72e20104
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-02 00:34:07 +0300

    Add upcoming info area and create image element in upcomingRender function

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -2,10 +2,16 @@ export const upComingRender = async film => {
   try {
     console.log('upcomingRender function called');
     console.log('film:', film);
+    const stand__area-upcoming
+    // Image elements
     const upcomingImage = document.createElement('img');
     upcomingImage.src = `https://image.tmdb.org/t/p/original/${film.backdrop_path}`;
     upcomingImage.alt = film.original_name || film.original_title;
-    upcomingImage.style.width
+    upcomingInfoArea.classList.add('stand__area-upcoming-image');
+
+    // UpComing Info Area
+    const upcomingInfoArea = document.createElement('div');
+    upcomingInfoArea.classList.add('stand__area-upcoming-info');
   } catch (error) {
     console.error('Error in upcomingRender:', error);
   }
